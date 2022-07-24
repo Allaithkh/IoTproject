e3332a2256a77bbf94323332e2ccfbfba98f6cb9
Allaithkh <97777362+Allaithkh@users.noreply.github.com>
2022-07-24 17:46:32 +0400

    Add files via upload

--- server.py
+++ server.py
@@ -1,37 +1,42 @@
-import socket 
+import socket
 import threading
 
-HEADER = 64
-PORT = 5050
-IMEI = 16
+PORT = 4033
 SERVER = socket.gethostbyname(socket.gethostname())
 ADDR = (SERVER, PORT)
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
 server.bind(ADDR)
 
 def handle_client(conn, addr):
-    print(f"[NEW CONNECTION] {addr} connected.")
-    print (conn)
-    print (ADDR)
-
-    connected = True
-    while connected:
-        msg_length = conn.recv(HEADER).decode(FORMAT)
-        if msg_length:
-            msg_length = int(msg_length)
-            msg = conn.recv(msg_length).decode(FORMAT)
-            if msg == DISCONNECT_MESSAGE:
-                  connected = False
-
-            print(f"[{addr}] {msg}")
-            conn.send("Msg received".encode(FORMAT))
-        print (msg)
-        
-    conn.close()
-    return msg   
+    while True:
+        data = conn.recv(4096)
+        msge =data.hex()
+        f = open("logs.txt", "a")
+        f.write(msge + '\n')
+        if (len(data) == 17):
+            print( f"received data from {addr}:", data)
+            ACK = 1
+            ack = ACK.to_bytes(1, byteorder= 'big', signed=True )
+            print("we send {}".format(ack))
+            conn.sendall(ack)
+        elif (len(data)>35):
+            print(  f"received data from {addr}:", msge)
+            resp = data[9]
+            print (resp)
+            res = resp.to_bytes(4, byteorder= 'big', signed=True )
+            print("we send {}".format(res))
+            conn.sendall(res)
+        else:
+            continue
+        if not conn:
+            conn.close()
+        f.close()
+    return msg
 
 def start():
     server.listen()
@@ -40,7 +45,7 @@ def start():
         conn, addr = server.accept()
         thread = threading.Thread(target=handle_client, args=(conn, addr))
         thread.start()
-        print(f"[ACTIVE CONNECTIONS] {threading.activeCount() - 1}")
+        print(f"[ACTIVE CONNECTIONS {addr}] {threading.active_count() - 1}")
 
 
 print("[STARTING] server is starting...")
